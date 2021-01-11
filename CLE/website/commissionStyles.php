7ff4d8356bb048d48fcdf867019ca4c06e1f70ef
Annie <a.janice.chen@gmail.com>
2021-01-11 15:54:17 +0100

    added content

--- CLE/website/commissionStyles.php
+++ CLE/website/commissionStyles.php
@@ -6,4 +6,17 @@
     <title>Commission Styles!</title>
     <link rel="stylesheet" href="../css/style.css">
 </head>
+<body>
+<div class="item centerTextAlign">
+    <p class="subtitle">Commission Styles</p>
+        <img class="images" src="../images/90sStyle.jpg"/>
+        <img class="images" src="../images/cartoonStyle.jpg"/>
+        <img class="images" src="../images/fullBodyStyle.jpg"/>
+    <p>
+    <a href="index.php">home</a>
+    <a href="forum.php">commission here!</a>
+    </p>
+</div>
+</body>
+</html>
 
