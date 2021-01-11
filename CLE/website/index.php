af7253a15b85bf23413dffb99dc9bed0cc95557f
Annie <a.janice.chen@gmail.com>
2021-01-11 10:57:54 +0100

    change in rules

--- CLE/website/index.php
+++ CLE/website/index.php
@@ -39,7 +39,9 @@
             <div class = "rules">
                 Terms of services
                 <br>
-                I will message you when I start working on your commission. There's 2 slots and I may take a maximum of 2 weeks on each commission, because of school. Please keep this in mind before purchasing.
+                I will message you when I start working on your commission.
+                There's 2 slots and Because of school I may take a maximum of 2 weeks on each commission.
+                Please keep this in mind before commissioning.
             </div>
         </div>
         <br>
